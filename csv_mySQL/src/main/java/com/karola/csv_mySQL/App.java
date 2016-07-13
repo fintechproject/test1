5b443520b8e0e730d4d78bb090d8c9d11ec0d2e8
carolyna06 <carolyna06@gmail.com>
2016-07-13 15:15:59 +0200

    our sample

---
+++ csv_mySQL/src/main/java/com/karola/csv_mySQL/App.java
@@ -0,0 +1,24 @@
+package com.karola.csv_mySQL;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+  public static void main(String[] args) {
+
+	String[] springConfig  = 
+		{	"spring/batch/config/database.xml", 
+			"spring/batch/config/context.xml",
+			"spring/batch/jobs/job-report.xml" 
+		};
+		
+	ApplicationContext context = 
+		new ClassPathXmlApplicationContext(springConfig);
+		
+
+  }
+}
---
+++ csv_mySQL/src/main/java/com/karola/csv_mySQL/Report.java
@@ -0,0 +1,66 @@
+package com.karola.csv_mySQL;
+
+public class Report {
+
+	private String Symbol;
+	private String Date;
+	private String Time;
+	private String Open;
+	private String High;
+	private String Low;
+	private String Close;
+	private String Volume;
+	
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	public String getOpen() {
+		return Open;
+	}
+	public void setOpen(String open) {
+		Open = open;
+	}
+	public String getHigh() {
+		return High;
+	}
+	public void setHigh(String high) {
+		High = high;
+	}
+	public String getLow() {
+		return Low;
+	}
+	public void setLow(String low) {
+		Low = low;
+	}
+	public String getClose() {
+		return Close;
+	}
+	public void setClose(String close) {
+		Close = close;
+	}
+	public String getVolume() {
+		return Volume;
+	}
+	public void setVolume(String volume) {
+		Volume = volume;
+	}
+	public String getSymbol() {
+		return Symbol;
+	}
+	public void setSymbol(String symbol) {
+		Symbol = symbol;
+	}
+	public String getTime() {
+		return Time;
+	}
+	public void setTime(String time) {
+		Time = time;
+	}
+	
+	
+
+	
+}
